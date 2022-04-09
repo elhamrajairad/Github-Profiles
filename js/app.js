ed8c992ee83ff8464aa00c917367b7d0d1d9d11e
elham rajaee <elham.rajairad@gmail.com>
2022-04-09 13:42:52 +0430

    add error handling in fetch

--- js/app.js
+++ js/app.js
@@ -7,33 +7,42 @@ btnUserName.addEventListener("change", (e) => {
   userID = e.target.value;
 });
 getBtn.addEventListener("click", () => {
-  fetch("https://api.github.com/users/" + userID)
-    .then((res) => res.json())
-    .then(function (res) {
-      return (content.innerHTML = `
+  if (!userID) {
+    return (errorNull.innerHTML = `Please enter user name !`);
+  } else {
+    fetch("https://api.github.com/users/" + userID)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(errorNull.innerHTML = `User ID ${Error.value}`);
+    })
+      .then(function (response) {
+        return (content.innerHTML = `
       <div class="profile__avatar">
-        <img src=${res.avatar_url} alt=${res.name}>
+        <img src=${response.avatar_url} alt=${response.name}>
       </div>
       <div class="profile__detaile">
         <div class="detaile__names">
-          <h1 class="profile__name">${res.name}</h1>
-          <p class="profile__id">ID: ${res.id}</p>
+          <h1 class="profile__name">${response.name}</h1>
+          <p class="profile__id">ID: ${response.id}</p>
         </div>
         <div class="detaile__user">
           <p class="user__repo">
-            <span class="repo__count">${res.public_repos}</span>
+            <span class="repo__count">${response.public_repos}</span>
             <span>Repository</span>
           </p>
           <p class="user__followers">
-            <span class="followers__count">${res.followers}</span>
+            <span class="followers__count">${response.followers}</span>
             <span>Followers</span>
           </p>
           <p class="user__following">
-            <span class="following__count">${res.following}</span>
+            <span class="following__count">${response.following}</span>
             <span>Following</span>
           </p>
         </div>
       </div>
     `);
-    });
+      });
+  }
 });
